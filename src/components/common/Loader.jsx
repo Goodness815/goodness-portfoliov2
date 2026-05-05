94a621498df1245426955e3579d75456621f55cb
Goodness Sewo <sewogoodness111@gmail.com>
2026-05-05 15:26:37 +0100

    feat: optimize SEO metadata, update PWA configuration with new favicons, and refine loader SVG scaling.

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -8,7 +8,7 @@ export const Loader = () => {
                 className="loader"
                 width="100%"
                 height="100%"
-                viewBox="0 0 800 200"
+                viewBox="0 0 400 200"
                 xmlns="http://www.w3.org/2000/svg"
             >
                 <text
